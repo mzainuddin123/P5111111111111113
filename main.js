5691433cead128fa0515c18717322cbf2de01181
mzainuddin123 <89520063+mzainuddin123@users.noreply.github.com>
2022-10-24 11:37:22 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function preload(){
+
+}
+function setup(){
+    
+        canvas=createCanvas(640,480);
+        canvas.position(110,300);
+        video=createCapture(VIDEO);
+        video.hide();
+}
+function draw(){
+    image(video,160,120,400,300);
+
+circle(30,30,60);
+circle(30,480,60);
+circle(610,30,60);
+circle(640,480,60);
+
+rect(56,20,560,50)
+
+fill(3,12,99);
+}
+function take_snap(){
+save("project.png");
+}
